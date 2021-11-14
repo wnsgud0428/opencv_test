c2226e42c04c8475b0178ec58cbdaa2ef1b451e0
wnsgud0428 <wnsgud0428@naver.com>
2021-11-15 04:41:28 +0900

    contour interpolation 시도중

--- curve_measure.py
+++ curve_measure.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy
+from scipy.interpolate import splprep, splev
 
 image = cv2.imread("media/wrong-crop.jpg_removebg.png")
 
@@ -16,6 +18,23 @@ hip_x = 250
 hip_y = 400
 cut = image[shoulder_y:hip_y, shoulder_x:hip_x]
 # cv2.imshow("cut", cut)
+
+smoothened = []
+for contour in contours:
+    x, y = contour.T
+    # Convert from numpy arrays to normal arrays
+    x = x.tolist()[0]
+    y = y.tolist()[0]
+    # https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.interpolate.splprep.html
+    tck, u = splprep([x, y], u=None, s=1.0, per=1)
+    # https://docs.scipy.org/doc/numpy-1.10.1/reference/generated/numpy.linspace.html
+    u_new = numpy.linspace(u.min(), u.max(), 23)
+    # https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.interpolate.splev.html
+    x_new, y_new = splev(u_new, tck, der=0)
+    # Convert it back to numpy format for opencv to be able to display it
+    res_array = [[[int(i[0]), int(i[1])]] for i in zip(x_new, y_new)]
+    smoothened.append(numpy.asarray(res_array, dtype=numpy.int32))
+cv2.drawContours(image, smoothened, -1, (255, 255, 255), 2)
 
 # 보여주는 부분
 cv2.imshow("output_image", output_image)
